371031cfcd3639e5a3f42cfc7bbfc2258706bbb5
Luciano <luciano.imbimbo01@gmail.com>
2023-02-28 10:00:30 +0100

    Icon and Logo

--- PasswordManager/src/main/java/com/code/passwordmanager/Main.java
+++ PasswordManager/src/main/java/com/code/passwordmanager/Main.java
@@ -3,6 +3,7 @@ package com.code.passwordmanager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -16,6 +17,7 @@ public class Main extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         String css = this.getClass().getResource("css/Menu.css").toExternalForm();
         scene.getStylesheets().add(css);
+        stage.getIcons().add(new Image("C:/Users/lucia/IdeaProjects/MyNewPasswordManager/PasswordManager/src/main/resources/com/code/passwordmanager/images/icon.png"));
         stage.setTitle("MyPasswordManager");
         stage.setScene(scene);
         stage.setResizable(false);
